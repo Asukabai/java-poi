c529f504b39881023628380e1f204ee1579b0f92
myai <>
2023-08-03 15:27:42 +0800

    项目初始化

---
+++ src/main/java/com/bai/javapoi/JavaPoiApplication.java
@@ -0,0 +1,13 @@
+package com.bai.javapoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPoiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bai/javapoi/JavaPoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bai.javapoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
